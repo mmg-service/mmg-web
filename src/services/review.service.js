55b1bf45de5687165684c5f753db6f5ecceed97e
정나린 <narin1101@gmail.com>
2025-04-30 09:48:18 +0900

    review

---
+++ src/services/review.service.js
@@ -0,0 +1,23 @@
+import { requestWithBaseURL } from './api'
+const reviewApi = requestWithBaseURL(process.env.VUE_APP_REVIEW_API_URL) 
+
+class ReviewService {
+  async getClaudeReview(queryParams) {
+    console.log('Using baseURL:', process.env.VUE_APP_REVIEW_API_URL)
+
+    const response = await reviewApi.get('/claude', {
+      params: queryParams
+    });
+    
+    if (response.data.token) {
+      localStorage.setItem('user-token', response.data.token)
+      localStorage.setItem('user', JSON.stringify(response.data.user))
+    }
+    
+    return response.data
+  }
+
+  
+}
+
+export default new ReviewService()
